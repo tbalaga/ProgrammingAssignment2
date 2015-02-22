8fe26d14d1f9fd03cfe7f50218d0cd64ff7f89d2
tbalaga <HB08832@LCNU235B8VJ.hbanc.hban.us>
2015-02-22 18:37:51 -0500

    HW2: Changes

--- cachematrix.R
+++ cachematrix.R
@@ -10,7 +10,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setinverse <- function(mean) m <<- mean
+        setinverse <- function(solve) m <<- solve
         getinverse <- function() m
         list(set = set, get = get,
              setinverse = setinverse,
@@ -22,13 +22,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		## get the inverse from x
+		
+		## check if the inverse is already calculated
 		m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+		
+		## inverse is not calculated/catched yet. Calculate inverse of the matrix
         data <- x$get()
         m <- solve(data, ...)
         x$setinverse(m)
         m
 }
+
+9a7d211e2b729f6188655af1972e20ef6a94a2dc
